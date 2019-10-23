1cccff5eaa5033191eb8c08081fd7a3d4006ed55
Janice Huang <janicehuang@gmail.com>
2019-10-22 20:41:34 -0700

    update test helper with SimpleCov and minitest

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,14 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require 'simplecov'
+SimpleCov.start do
+  add_filter 'test/'
+end
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/reporters'
+require 'minitest/pride'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
