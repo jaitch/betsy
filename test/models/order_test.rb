fc678053629d2ff4f8eb0abaf1d0c524c1c18f4c
Janice Huang <janicehuang@gmail.com>
2019-10-23 15:29:48 -0700

    add relation model test for order

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -4,13 +4,16 @@ describe Order do
   describe 'validations' do
     it 'can be made with valid input' do
     end
-
-    it 
   end
-
+  
   describe 'relations' do
+    it 'has products' do
+      order = order(:c)
+      order.products.length.must_equal 2
+    end
   end
-
+  
+  
   describe 'custom methods' do
   end
 end
