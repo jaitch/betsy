8fa482e8ee246f5a08d59a05c4eb6e0b34f64767
Julia A Kingrey <juliakingreyworks@gmail.com>
2019-10-22 17:45:03 -0700

    Added product loop to seeds.rb and changed name of product csv file to match existing files.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,67 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+MERCHANT_FILE = Rails.root.join('db', 'merchants-seeds.csv')
+CATEGORY_FILE = Rails.root.join('db', 'categories-seeds.csv')
+PRODUCT_FILE = Rails.root.join('db', 'products-seeds.csv')
+
+puts "Loading raw media data from #{MERCHANT_FILE}"
+
+merchants_failures = []
+
+CSV.foreach(MERCHANT_FILE, :headers => true) do |row|
+  merchant = Merchant.new
+  merchant.username = row['username']
+  merchant.email = row['email']
+  successful = merchant.save
+  
+  if !successful
+    merchants_failures << merchant
+    puts "Failed to save merchant: #{merchant.errors.inspect}"
+  else
+    puts "Created merchant: #{merchant.inspect}"
+  end
+end
+
+
+puts "Loading raw media data from #{CATEGORY_FILE}"
+categories_failures = []
+
+CSV.foreach(MERCHANT_FILE, :headers => true) do |row|
+  category = Category.new
+  category.name = row['name']
+  successful = category.save
+  
+  if !successful
+    category_failures << category
+    puts "Failed to save merchant: #{category.errors.inspect}"
+  else
+    puts "Created merchant: #{category.inspect}"
+  end
+end
+
+puts "Loading raw media data from #{PRODUCT_FILE}"
+
+products_failures = []
+
+CSV.foreach(PRODUCT_FILE, :headers => true) do |row|
+  product = Product.new
+  product.name = row['name']
+  product.price = row['price']
+  product.stock = row['stock']
+  product.description = row['description']
+  product.photourl = row['photourl']
+  product.retired = row ['retired?']
+  successful = product.save
+  
+  if !successful
+    productss_failures << product
+    puts "Failed to save product: #{product.errors.inspect}"
+  else
+    puts "Created merchant: #{product.inspect}"
+  end
+end
+
