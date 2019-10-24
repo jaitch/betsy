0bb4bf363be3c634231e34f9e60d26224d84ad5e
Mariya Burrows <mariya.burrows@gmail.com>
2019-10-24 10:09:00 -0700

    added new/create product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,4 +16,17 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
   end
+  
+  def create
+    @product = Product.new(name: params[:product][:name], price: params[:product][:price], stock: params[:product][:stock], description: params[:product][:description], photo: params[:product][:photo], retire: params[:product][:retire], merchant_id: 1)
+    if @product.save 
+      flash[:success] = "Your product has been added."
+      redirect_to root_path
+      return
+    else 
+      flash.now[:warning] = "Your product could not be saved because #{@product.errors.full_messages}"
+      render :new
+      return
+    end
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,11 +4,35 @@ class Product < ApplicationRecord
   has_many :orderproducts
   has_and_belongs_to_many :categories
   has_many :orders, through: :orderproducts
-
+  
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :stock, presence: true, numericality: { only_integer: true, greater_than: -1 }
-
-
+  
+  # pending custom methods, waiting on categories and orderproducts models to complete
+  # def self.products_by_category
+  #   categories = []
+  
+  #   products = self.all
+  
+  #   products.each do |product|
+  #     if !(categories.include?(product.categories_products))
+  #       categories << product
+  #     end
+  #   end
+  
+  #   return categories
+  # end
+  
+  # def self.products_by_merchant 
+  # end
+  
+  # def update_inventory(orderproducts)
+  #   quanity = orderproducts.quanity 
+  #   self.stock = stock - quanity
+  #   return quantity 
+  # end
+  
+  
 end
