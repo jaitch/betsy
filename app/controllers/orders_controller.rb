380127060f52034a1efdd01da2a086c6cea4e58b
Janice Huang <janicehuang@gmail.com>
2019-10-24 08:28:01 -0700

    add strong params and new method, start create method in order controller; add cvc to form

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,29 @@
 class OrdersController < ApplicationController
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+  end
+
   def delete
+  end
+
+  private
+  def order_params
+    return params.require(:order).permit(:name, :email, :mailing_address, :zip, :name_on_cc, :cc_number, :cc_cvc, :cc_exp)
+  end
+
+  def find_order
+    @order = Order.find_by(id: params[:id])
+  end
+
+  def if_order_missing
+    if @order.nil?
+      flash[:error] = "Order with id #{params[:id]} was not found!"
+      redirect_to works_path
+      return
+    end
   end
 end
