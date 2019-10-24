1fa5cdee022f97c28c461f5c447fcc58dcc02101
Janice Huang <janicehuang@gmail.com>
2019-10-23 22:32:33 -0700

    use session functionality to enable adding products to cart, initiating orders or updating orders, flashing success/failure msgs, creating orderproduct instances

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :orders#, except: [:put]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/cart/:id', to: 'application#add_to_cart' , as: 'cart'
 
   root "products#index"
 
