83d64b57fa5b97ce6e37a11cca8db077897985d8
Julia A Kingrey <juliakingreyworks@gmail.com>
2019-10-25 14:33:08 -0700

    Added reviews controller correctly.

--- app/controllers/reviews_controller_controller.rb
+++
@@ -1,2 +0,0 @@
-class ReviewsControllerController < ApplicationController
-end
--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-describe ReviewsControllerController do
+describe ReviewsController do
   # it "does a thing" do
   #   value(1+1).must_equal 2
   # end
