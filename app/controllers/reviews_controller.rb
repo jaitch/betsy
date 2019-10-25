080b5a928356825c34921d256de8fe3e60556583
Julia A Kingrey <juliakingreyworks@gmail.com>
2019-10-25 14:41:24 -0700

    Added strong params to reviews controller.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,39 @@
 class ReviewsController < ApplicationController
+  def index 
+    @reviews = Review.all
+  end
+  
+  def show 
+    review_id = params[:id]
+    @review = Review.find_by(id: review_id)
+    
+    if @review.nil?
+      redirect_to root_path
+      # redirect to product page?
+    end
+  end
+  
+  def new
+    @review = Review.new
+  end
+  
+  def create
+    @review = Review.new
+    if @review.save 
+      flash[:success] = "Your review has been saved."
+      redirect_to root_path
+      return
+    else 
+      flash.now[:warning] = "Your review could not be saved because #{@review.errors.full_messages}"
+      render :new
+      return
+    end
+  end
+
+  private 
+
+  def review_params
+    params.require(:review).permit(:rating, :text)
+  end
+
 end
