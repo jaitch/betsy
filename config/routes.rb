c42ff8ceee606a380065f1a89b32d1bcc7d6435b
Julia A Kingrey <juliakingreyworks@gmail.com>
2019-10-27 13:11:30 -0700

    Added nested routes under products for reviews.

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,27 @@
 Rails.application.routes.draw do
   resources :orders#, except: [:put]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-
+  
+  
   root "homepages#index"
-
+  
   get '/cart/:id', to: 'application#add_item' , as: 'cart'
-
-  resources :products
+  
+  resources :products do
+    resources :reviews, only: [:index, :new, :create]
+  end
 
   resources :merchants
-
+  
   resources :categories
-
+  
+  
   # get "/login", to: "merchants#login_form", as: "login"
   # post "/login", to: "merchants#login"
   # post "/logout", to: "merchants#logout", as: "logout"
-
+  
   get "/merchants/current", to: "merchants#current", as: "current_merchant"
-
+  
   get "/auth/github", as: "github_login"
   get "/auth/:provider/callback", to: "merchants#create", as: "auth_callback"
   delete "/logout", to: "merchants#destroy", as: "logout"
