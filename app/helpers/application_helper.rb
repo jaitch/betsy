bd50d07298b6226bad0d3d0c270c9e1d6b55dd17
Janice Huang <janicehuang@gmail.com>
2019-10-27 21:16:53 -0700

    add view helper for date/time of order placed on confirmation page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,27 +1,38 @@
 module ApplicationHelper
   def merchants_dropdown
     "<li class=\"nav-item dropdown\">".html_safe +
-       "<a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"navbarDropdown\" role=\"button\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">".html_safe +
-          "Merchants".html_safe +
-        "</a>".html_safe +
-        "<div class=\"dropdown-menu\" aria-labelledby=\"navbarDropdown\">".html_safe +
-          Merchant.all.map do |merchant| 
-            link_to merchant.username, merchant_path(merchant.id), class:"dropdown-item" 
-          end.join(" ").html_safe +
-        "</div>".html_safe +
-      "</li>".html_safe 
+    "<a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"navbarDropdown\" role=\"button\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">".html_safe +
+    "Merchants".html_safe +
+    "</a>".html_safe +
+    "<div class=\"dropdown-menu\" aria-labelledby=\"navbarDropdown\">".html_safe +
+    Merchant.all.map do |merchant|
+      link_to merchant.username, merchant_path(merchant.id), class:"dropdown-item"
+    end.join(" ").html_safe +
+    "</div>".html_safe +
+    "</li>".html_safe
   end
 
   def categories_dropdown
     "<li class=\"nav-item dropdown\">".html_safe +
-       "<a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"navbarDropdown\" role=\"button\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">".html_safe +
-          "Categories".html_safe +
-        "</a>".html_safe +
-        "<div class=\"dropdown-menu\" aria-labelledby=\"navbarDropdown\">".html_safe +
-          Category.all.map do |category| 
-            link_to category.name, category_path(category.id), class:"dropdown-item" 
-          end.join(" ").html_safe +
-        "</div>".html_safe +
-      "</li>".html_safe 
+    "<a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"navbarDropdown\" role=\"button\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">".html_safe +
+    "Categories".html_safe +
+    "</a>".html_safe +
+    "<div class=\"dropdown-menu\" aria-labelledby=\"navbarDropdown\">".html_safe +
+    Category.all.map do |category|
+      link_to category.name, category_path(category.id), class:"dropdown-item"
+    end.join(" ").html_safe +
+    "</div>".html_safe +
+    "</li>".html_safe
+  end
+  
+  def readable_date(date)
+    return "[unknown]" unless date
+    return (
+      "<span class='date' title='".html_safe +
+      date.to_s +
+      "'>".html_safe +
+      time_ago_in_words(date) +
+      " ago</span>".html_safe
+    )
   end
 end
