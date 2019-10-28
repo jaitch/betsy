ae9fb1ddf96078caa0a836e1550d79c83dc234b5
Janice Huang <janicehuang@gmail.com>
2019-10-28 14:27:51 -0700

    finish form within order show page to update quantity and save in orderproduct

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  
-  resources :orders#, except: [:put] do
+
+  resources :orders do
+    resources :orderproducts, only: [:update]
+  end
   get "/confirmation/:id", to: "orders#confirmation", as: "confirmation"
 
   resources :orderproducts, except: [:create]
@@ -10,20 +12,20 @@ Rails.application.routes.draw do
   resources :products do
     resources :orderproducts, only: [:create, :destroy]
     resources :reviews, only: [:new, :create]
-  end 
+  end
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html  
-  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   resources :merchants
-  
+
   resources :categories
-  
+
   # get "/login", to: "merchants#login_form", as: "login"
   # post "/login", to: "merchants#login"
   # post "/logout", to: "merchants#logout", as: "logout"
-  
+
   get "/merchants/current", to: "merchants#current", as: "current_merchant"
-  
+
   get "/auth/github", as: "github_login"
   get "/auth/:provider/callback", to: "merchants#create", as: "auth_callback"
   delete "/logout", to: "merchants#destroy", as: "logout"
