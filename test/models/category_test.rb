eba9f6e277522e8e06f35a95428276b556714d21
Mariya Burrows <mariya.burrows@gmail.com>
2019-10-29 15:24:49 -0700

    added category testing

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,38 @@
 require "test_helper"
 
 describe Category do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "relationships" do 
+    it "can retrieve products related to its category" do 
+      # Arrange
+      puppy = categories(:puppy)
+      
+      # Act - Assert
+      expect(puppy.products.first).must_be_kind_of Product
+      expect(puppy.products.first.name).must_equal "batman"
+    end
+
+    it "can add a product through its category" do 
+      # Arrange
+      cat = Category.new(name: "cat")
+      
+      # Act
+      mice = Product.new(name: "mice", merchant_id: 1, stock: 3, price: 12.99, description: "abc", photo: nil, retire: false)
+      cat.products << mice 
+      
+      
+      # Assert
+      expect(cat.products.first).must_be_kind_of Product
+      expect(cat.products.first.name).must_equal "mice"
+      
+    end
+  end
+  
+  describe "validations" do 
+    it "has a name" do 
+      
+    end
+    
+    it "has a unique name" do 
+    end
+  end
 end
