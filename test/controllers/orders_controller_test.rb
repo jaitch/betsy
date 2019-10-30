81f91633e2f070feb8053319c1e0d146e5f50942
Janice Huang <janicehuang@gmail.com>
2019-10-30 14:03:24 -0700

    get update stock quantity test to FINALLY work

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -33,17 +33,17 @@ describe OrdersController do
       mailing_address: "123 Pine St.",
       zip: 12345,
       name_on_cc: "Tummy B. Button",
-      cc_number: "252024045",
-      cc_exp: "11/9/19"
-      }}
+      cc_number: 2520240451111111,
+      cc_exp: "11/9/19",
+      cc_cvc: 345,
+      status: "paid"}}
       expect{ patch order_path(order.id), params: checkout_hash }.wont_change Order.count
-      must_respond_with :success
-      # tried to check redirect to confirmation page, but it's hitting the success reponse first
+      must_redirect_to confirmation_path
+
     end
 
     it 'decreases quantity purchased from product stock' do
-      valid_product = Product.last
-      valid_product.stock = 5
+      valid_product = products(:clown)
       cur_id = valid_product.id
       post product_orderproducts_path(cur_id)
       order = Order.last
@@ -54,12 +54,15 @@ describe OrdersController do
       mailing_address: "123 Pine St.",
       zip: 12345,
       name_on_cc: "Tummy B. Button",
-      cc_number: "252024045",
-      cc_exp: "11/19"
-      }}
-      patch order_path(order.id), params: checkout_hash
-      updated_valid_product = Product.find_by(id: cur_id)
-      expect(updated_valid_product.stock).must_equal 4
+      cc_number: 2520240451111111,
+      cc_exp: "11/19",
+      cc_cvc: 345,
+      status: "paid"}}
+      expect{ patch order_path(order.id), params: checkout_hash }.wont_change Order.count
+      updated_order = Order.find_by(id: order.id)
+      updated_product = updated_order.products.first
+      expect(updated_order.name).must_equal checkout_hash[:order][:name]
+      expect(updated_product.stock).must_equal 19
     end
     # Was going to test that status changed from 'pending' to 'paid' but that happens with a hidden field in the form, so not appropriate to test?
   end
