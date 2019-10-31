f0f279f021a4f5151c3ee69b332eb0cb9c5849b4
Ga-Young Jin <gyjinn@gmail.com>
2019-10-31 14:47:53 -0700

    formatted all products in manage store view page

--- config/routes.rb
+++ config/routes.rb
@@ -20,10 +20,6 @@ Rails.application.routes.draw do
   
   resources :categories
   
-  # get "/login", to: "merchants#login_form", as: "login"
-  # post "/login", to: "merchants#login"
-  # post "/logout", to: "merchants#logout", as: "logout"
-  
   get "/merchants/current", to: "merchants#current", as: "current_merchant"
   
   get "/auth/github", as: "github_login"
