90680be6fd15ad6cfc153506b715ef31640a69ec
Janice Huang <janicehuang@gmail.com>
2019-10-30 20:33:18 -0700

    set up order model test validations and fix yml cc numbers

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -2,8 +2,34 @@ require "test_helper"
 
 describe Order do
   describe 'validations' do
+  before do
+  order = orders(:a)
+  end
     it 'can be made with valid input' do
+
     end
+    it 'on update, is invalid without a name' do
+    end
+it 'on update, is invalid without an email' do
+  end
+it 'on update, is invalid without a mailing address' do
+end
+
+it 'on update, is invalid without a five-digit integer zip code' do
+end
+
+it 'on update, is invalid without a credit card name' do
+end
+
+it 'on update, is invalid without a sixteen-digit credit card number' do
+end
+
+it 'on update, is invalid without a three-digit integer credit card cvc' do
+end
+
+it 'on update, is invalid without a credit card expiration date' do
+end
+
   end
 
   describe 'relations' do
