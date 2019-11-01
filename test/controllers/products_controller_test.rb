2fea10ec555bf1506f8fabe5114d8514a42380bf
Mariya Burrows <mariya.burrows@gmail.com>
2019-11-01 13:00:41 -0700

    cleaned up products files

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -256,19 +256,6 @@ describe ProductsController do
         @magician.stock.must_equal 12
         @magician.retire.must_equal true
       end 
-      
-      # it "cannot update another merchant's product" do 
-      #   # Arrange
-      #   @batman = products(:batman)
-        
-      #   #Act
-      #   get edit_product_path(@batman.merchant)
-        
-      #   #Assert
-      #   must_respond_with :redirect
-      #   expect(flash[:warning]).wont_be_nil
-      #   must_redirect_to root_path
-      # end
 
       it "cannot edit an invalid product" do 
         # Act
